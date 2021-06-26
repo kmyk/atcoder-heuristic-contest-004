6403bf23a55743d81b75a71e0e668a244f1004ef
Kimiyuki Onaka <kimiyuki95@gmail.com>
2021-06-26 13:50:26 +0900

    initial solution

--- main.cpp
+++ main.cpp
@@ -29,75 +29,64 @@ private:
     uint32_t a, b, c, d;
 };
 
-constexpr int N = 50;
+constexpr int N = 20;
+constexpr int LEN_MIN = 2;
+constexpr int LEN_MAX = 12;
 
-constexpr array<int, 4> DIRS = {{0, 1, 2, 3}};
-constexpr array<int, 4> DIR_Y = {-1, 1, 0, 0};
-constexpr array<int, 4> DIR_X = {0, 0, 1, -1};
-inline bool is_on_tiles(int y, int x) {
-    return 0 <= y and y < N and 0 <= x and x < N;
+array<array<char, N>, N> get_empty_board() {
+    array<array<char, N>, N> f;
+    REP (y, N) {
+        fill(ALL(f[y]), '.');
+    }
+    return f;
 }
 
-inline uint16_t pack_point(int y, int x) {
-    return (y << 8) + x;
-}
-
-inline pair<int, int> unpack_point(int packed) {
-    return {packed >> 8, packed & ((1 << 8) - 1)};
-}
-
-string convert_to_command_string(const vector<uint16_t>& result) {
-    assert (not result.empty());
-    string ans;
-    REP (i, (int)result.size() - 1) {
-        auto [ay, ax] = unpack_point(result[i]);
-        auto [by, bx] = unpack_point(result[i + 1]);
-        if (by == ay - 1 and bx == ax) {
-            ans.push_back('U');
-        } else if (by == ay + 1 and bx == ax) {
-            ans.push_back('D');
-        } else if (by == ay and bx == ax + 1) {
-            ans.push_back('R');
-        } else if (by == ay and bx == ax - 1) {
-            ans.push_back('L');
-        } else {
-            assert (false);
+bool is_horizontal_subarray_at(const string& s, int y, int x, const array<array<char, N>, N>& f) {
+    REP (i, s.size()) {
+        if (s[i] != f[y][(x + i) % N]) {
+            return false;
         }
     }
-    return ans;
+    return true;
+}
+
+bool is_vertical_subarray_at(const string& s, int y, int x, const array<array<char, N>, N>& f) {
+    REP (i, s.size()) {
+        if (s[i] != f[(y + i) % N][x]) {
+            return false;
+        }
+    }
+    return true;
+}
+
+string get_horizontal_subarray_at(int y, int x, int len, const array<array<char, N>, N>& f) {
+    string s(len, '\0');
+    REP (i, len) {
+        s[i] = f[y][(x + i) % N];
+    }
+    return s;
+}
+
+string get_vertical_subarray_at(int y, int x, int len, const array<array<char, N>, N>& f) {
+    string s(len, '\0');
+    REP (i, len) {
+        s[i] = f[(y + i) % N][x];
+    }
+    return s;
 }
 
 template <class RandomEngine>
-string solve(const int sy, const int sx, const array<array<int, N>, N>& tile, const array<array<int, N>, N>& point, RandomEngine& gen, chrono::high_resolution_clock::time_point clock_end) {
+array<array<char, N>, N> solve(const int m, const vector<string>& s, RandomEngine& gen, chrono::high_resolution_clock::time_point clock_end) {
     chrono::high_resolution_clock::time_point clock_begin = chrono::high_resolution_clock::now();
 
-    int M = 0;
-    REP (y, N) {
-        REP (x, N) {
-            M = max(M, tile[y][x] + 1);
-        }
+    array<array<char, N>, N> ans = get_empty_board();
+    pair<int, int> highscore = make_pair(- m, N * N);  // (m - c, d)
+
+    unordered_map<string, vector<int>> occur;
+    REP (i, m) {
+        occur[s[i]].push_back(i);
     }
 
-    vector<uint16_t> path_prev;
-    path_prev.push_back(pack_point(sy, sx));
-    vector<int16_t> used_tile_prev(M, INT16_MAX);
-    used_tile_prev[tile[sy][sx]] = 0;
-    array<array<bool, N>, N> used_pos_prev = {};
-    used_pos_prev[sy][sx] = true;
-    vector<int> score_prev;
-    score_prev.push_back(0);
-    score_prev.push_back(point[sy][sx]);
-
-    vector<uint16_t> result = path_prev;
-    int highscore = score_prev.back();
-#ifdef VISUALIZE
-    int highscore_index = 0;
-    cerr << "-----BEGIN-----" << endl;
-    cerr << convert_to_command_string(result) << endl;
-    cerr << "-----END-----" << endl;
-#endif  // VISUALIZE
-
-    // simulated annealing
     int64_t iteration = 0;
     double temperature = 1.0;
     for (; ; ++ iteration) {
@@ -110,135 +99,102 @@ string solve(const int sy, const int sx, const array<array<int, N>, N>& tile, co
             }
         }
 
-        int start = uniform_int_distribution<int>(1, path_prev.size())(gen);
-        vector<char> used_tile_next(M);
-        auto get_used_tile = [&](int y, int x) {
-            int i = tile[y][x];
-            return used_tile_prev[i] < start or used_tile_next[i];
-        };
-        vector<uint16_t> diff;
-        int score_next = score_prev[start];
-        auto [y, x] = unpack_point(path_prev[start - 1]);
-        while (true) {
-            array<int, 4> dirs = {{0, 1, 2, 3}};
-            shuffle(ALL(dirs), gen);
-            bool found = false;
-            for (int dir : dirs) {
-                int ny = y + DIR_Y[dir];
-                int nx = x + DIR_X[dir];
-                if (not is_on_tiles(ny, nx)) {
-                    continue;
-                }
-                if (diff.empty() and start < path_prev.size() and path_prev[start] == pack_point(ny, nx)) {
-                    continue;
-                }
-                if (not get_used_tile(ny, nx)) {
-                    found = true;
-                    diff.push_back(pack_point(ny, nx));
-                    y = ny;
-                    x = nx;
-                    used_tile_next[tile[y][x]] = true;
-                    score_next += point[y][x];
+        array<array<char, N>, N> cur = get_empty_board();
+        int d = N * N;
+        vector<bool> used(m);
+        int y = 0;
+        int x = 0;
+
+        vector<int> order(m);
+        iota(ALL(order), 0);
+        shuffle(ALL(order), gen);
+        for (int i : order) {
+            if (used[i]) {
+                continue;
+            }
+            int offset = -1;
+            REP_R (k, (int)s[i].size()) {
+                if (x - k >= 0 and x - k + s[i].size() <= N and is_horizontal_subarray_at(s[i].substr(0, k), y, x - k, cur)) {
+                    offset = k;
                     break;
                 }
             }
-            if (not found) {
-                break;
-            }
-            if (used_pos_prev[y][x]) {
-                break;
-            }
-        }
-        if (diff.empty()) {
-            continue;
-        }
-        int tail_first = path_prev.size();
-        int tail_last = path_prev.size();
-        if (used_pos_prev[y][x]) {
-            tail_first = start;
-            while (tail_first < path_prev.size() and path_prev[tail_first] != pack_point(y, x)) {
-                ++ tail_first;
-            }
-            assert (tail_first < path_prev.size());
-            ++ tail_first;
-            REP3 (i, tail_first, path_prev.size()) {
-                auto [y, x] = unpack_point(path_prev[i]);
-                if (get_used_tile(y, x)) {
-                    tail_last = i;
+            if (offset == -1) {
+                ++ y;
+                x = 0;
+                if (y >= N) {
                     break;
                 }
-                used_tile_next[tile[y][x]] = true;
+                offset = 0;
+            }
+            REP3 (z, offset, s[i].size()) {
+                assert (x < N);
+                cur[y][x] = s[i][z];
+
+                REP3 (len, LEN_MIN, LEN_MAX + 1) {
+                    if (x - len >= 0) {
+                        string t = get_horizontal_subarray_at(y, x - len, len, cur);
+                        auto it = occur.find(t);
+                        if (it != occur.end()) {
+                            for (int j : it->second) {
+                                used[j] = true;
+                            }
+                        }
+                    }
+                    if (y - len >= 0) {
+                        string t = get_vertical_subarray_at(y - len, x, len, cur);
+                        auto it = occur.find(t);
+                        if (it != occur.end()) {
+                            for (int j : it->second) {
+                                used[j] = true;
+                            }
+                        }
+                    }
+                }
+
+                x += 1;
+                d -= 1;
             }
         }
-        score_next += score_prev[tail_last] - score_prev[tail_first];
 
-        int delta = score_next - score_prev.back();
-        auto probability = [&]() {
-            constexpr long double boltzmann = 0.01;
-            return exp(boltzmann * delta / temperature);
-        };
-        if (delta >= 0 or bernoulli_distribution(probability())(gen)) {
-            // accept
-            if (delta < 0) {
-#ifdef VERBOSE
-                cerr << "decreasing move  (delta = " << delta << ", iteration = " << iteration << ")" << endl;
-#endif  // VERBOSE
-            }
-
-            diff.insert(diff.end(), path_prev.begin() + tail_first, path_prev.begin() + tail_last);
-            path_prev.resize(start);
-            path_prev.insert(path_prev.end(), ALL(diff));
-            used_tile_prev.assign(M, INT16_MAX);
-            used_pos_prev = {};
-            score_prev.clear();
-            score_prev.push_back(0);
-            REP (i, path_prev.size()) {
-                auto [y, x] = unpack_point(path_prev[i]);
-                used_tile_prev[tile[y][x]] = i;
-                used_pos_prev[y][x] = true;
-                score_prev.push_back(score_prev.back() + point[y][x]);
-            }
-
-            if (highscore < score_prev.back()) {
-                highscore = score_prev.back();
-                result = path_prev;
-#ifdef VERBOSE
-                cerr << "highscore = " << highscore << "  (iteration = " << iteration << ")" << endl;
-#endif  // VERBOSE
-#ifdef VISUALIZE
-                cerr << "-----BEGIN-----" << endl;
-                cerr << convert_to_command_string(result) << endl;
-                cerr << "-----END-----" << endl;
-#endif  // VISUALIZE
-            }
+        int c = count(ALL(used), true);
+        pair<int, int> score = make_pair(m - c, d);
+        if (highscore < score) {
+            highscore = score;
+            ans = cur;
         }
     }
 
-    string ans = convert_to_command_string(result);
-    cerr << "ans = " << ans << endl;
-    cerr << "score = " << highscore << endl;
+    cerr << "ans =" << endl;
+    cerr << "m = " << m << endl;
+    REP (y, N) {
+        cerr << "    ";
+        REP (x, N) {
+            cerr << ans[y][x];
+        }
+        cerr << endl;
+    }
+    cerr << "score = (c, d) = (" << m - highscore.first << ", " << highscore.second << ")" << endl;
     return ans;
 }
 
 int main() {
-    constexpr auto TIME_LIMIT = chrono::milliseconds(2000);
+    constexpr auto TIME_LIMIT = chrono::milliseconds(3000);
     chrono::high_resolution_clock::time_point clock_begin = chrono::high_resolution_clock::now();
     xor_shift_128 gen(20210425);
 
-    int sy, sx; cin >> sy >> sx;
-    array<array<int, N>, N> tile;
+    int n, m; cin >> n >> m;
+    assert (n == N);
+    vector<string> s(m);
+    REP (i, m) {
+        cin >> s[i];
+    }
+    array<array<char, N>, N> ans = solve(m, s, gen, clock_begin + chrono::duration_cast<chrono::milliseconds>(TIME_LIMIT * 0.95));
     REP (y, N) {
         REP (x, N) {
-            cin >> tile[y][x];
+            cout << ans[y][x];
         }
+        cout << endl;
     }
-    array<array<int, N>, N> point;
-    REP (y, N) {
-        REP (x, N) {
-            cin >> point[y][x];
-        }
-    }
-    string ans = solve(sy, sx, tile, point, gen, clock_begin + chrono::duration_cast<chrono::milliseconds>(TIME_LIMIT * 0.95));
-    cout << ans << endl;
     return 0;
 }
